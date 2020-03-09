2af0b701cc7127196ec93509468f1affa53f8bf9
Kevin Dean <kadean08@gmail.com>
2020-03-09 15:13:58 -0400

    add to quoteCards

--- server/models/quoteCards.js
+++ server/models/quoteCards.js
@@ -1,9 +1,17 @@
 const quoteCards = [
-    "This class is awesome",
-    "Come to Shabbat 200"
+    'It\'s a trap!',
+    'Soooon'
 ];
+
+function add(text){
+    quoteCards.push(text);
+}
 
 //commonJS way of doing modules
 //this is a "default export" b/c just setting equal to var
 //could also have module.exports.something which would return an object that has a property of quoteCards
-module.exports = quoteCards;
+module.exports = {
+    list: quoteCards,
+    add: add
+}
+
