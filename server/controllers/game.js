eb2623599e8412756d177b257a20b2891a5dc2d3
Kevin Dean <kadean08@gmail.com>
2020-03-12 15:01:35 -0400

    using json parsing middleware

--- server/controllers/game.js
+++ server/controllers/game.js
@@ -7,10 +7,17 @@ const quoteCards = require('../models/quoteCards');
 const router = express.Router();
 
 router
+    .use('/quoteCards', (req, res, next) => {
+        console.log({
+            method: req.method, body: req.body
+        });
+        next();
+    })
     .get('/quoteCards', (req, res) => res.send(quoteCards))
     .post('/quoteCards', (req, res) => {
         quoteCards.add(req.query.text);
         res.send(quoteCards.list[quoteCards.list.length - 1]);
-    });
+    })
+
 
 module.exports = router;
--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const app = express();
 const port = 3000;
 
 app
+    .use(express.json())
+    .use(express.urlencoded({extended: true})) //need this if handling regular form submissions
     .get('/', (req, res) => res.send('Hello Expansive Universe!'))
     .use('/game', gameController);
  
